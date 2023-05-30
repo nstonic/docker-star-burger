3eef94f98b04a77531433292475ca00fda52f845
nstonic <nstonic@gmail.com>
2023-05-30 15:37:05 +0400

    Set yaml

--- backend/star_burger/settings.py
+++ backend/star_burger/settings.py
@@ -85,7 +85,7 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=env('DB_URL', 'sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
+        default=env('DJANGO_DB_URL', 'sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
     )
 }
 
